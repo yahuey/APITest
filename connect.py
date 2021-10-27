5416c84b0983f963cd0c85027e37037fe2ba7937
Javier Benitez <javier.benitez00@gmail.com>
2021-10-27 10:38:01 -0600

    Creating all the variables to get everything started. With all the endpoints

--- connect.py
+++ connect.py
@@ -1,3 +1,5 @@
-# variable here because they
+# variable here because in case of need to change API, it's only in one place
 api_key = "b7fd41a703f1568133d18d5f5c46f262"
-
+api_base_url = "https://api.themoviedb.org/3"
+movie_id = 0
+endpoint_path = f"/movie/{movie_id}"
