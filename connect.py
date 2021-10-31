a58c6a368bfa2f12cad18cf2a1b18b434e3320ad
yahuey <30640620+yahuey@users.noreply.github.com>
2021-10-31 15:50:49 -0600

    Trying to do something cool here it is not working

--- connect.py
+++ connect.py
@@ -29,7 +29,8 @@ def movie_query(new_keyword):
     api_base_url = "https://api.themoviedb.org/3"
     endpoint_path = f"/search/movie"
     # again creating an endpoint for the movies
-    endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page=1"
+    page_number = 2
+    endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page={page_number}"
     req1 = requests.get(endpoint)
     # for troubleshooting purposes
     print(req1.status_code)
@@ -37,15 +38,30 @@ def movie_query(new_keyword):
     init_json = req1.text
     # Then saving the JSON data into a dictionary for easier navigation
     init_dict = json.loads(init_json)
-    # Showing the data row-by-row in this format
-    for x, y in init_dict.items():
-        print(x, y)
-    # This is to get a specific ID that we need. I really wanted to test my knowledge of nested lists and dictionaries.
-    return init_dict
+    final_dict = init_dict["results"]
+    final_dict
+    # I have to stop here, but I am trying to get all of the pages here into one CSV form
+    loop_dict = {}
+    # loop_json = ""
+    # if int(init_dict["total_pages"]) > page_number:
+    #     count = int(init_dict["total_pages"])
+    #     print("This is count " + str(count))
+    #     for page_number in range(count):
+    #         loop_endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page={page_number}"
+    #         loop_req = requests.get(loop_endpoint)
+    #         loop_json = loop_req.text
+    #         loop_dict.update(loop_json)
+    #         final_dict.update(loop_dict["results"])
+    #         loop_dict.clear()
+    #         loop_json = ""
+    #         page_number += 1
+    #     return final_dict
+    # else:
+    return final_dict
 
 # Here we are looking at the results only for a cleaner search, then saving them as a csv format to be looked at.
 def display_data(search_dict):
-    df = pd.DataFrame(search_dict["results"])
+    df = pd.DataFrame(search_dict).rem
     file_export = df.to_csv()
     print(df)
     return file_export
