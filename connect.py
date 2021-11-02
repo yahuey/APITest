5c92fdd0f7be177f186b4fd3aa72630e7edf49dc
Javier Benitez <javier.benitez00@gmail.com>
2021-11-01 23:32:26 -0600

    This is a testament to will power. This will now take in all the pages into one CSV file.

--- connect.py
+++ connect.py
@@ -29,7 +29,7 @@ def movie_query(new_keyword):
     api_base_url = "https://api.themoviedb.org/3"
     endpoint_path = f"/search/movie"
     # again creating an endpoint for the movies
-    page_number = 2
+    page_number = 1
     endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page={page_number}"
     req1 = requests.get(endpoint)
     # for troubleshooting purposes
@@ -38,30 +38,49 @@ def movie_query(new_keyword):
     init_json = req1.text
     # Then saving the JSON data into a dictionary for easier navigation
     init_dict = json.loads(init_json)
-    final_dict = init_dict["results"]
-    final_dict
-    # I have to stop here, but I am trying to get all of the pages here into one CSV form
-    loop_dict = {}
-    # loop_json = ""
-    # if int(init_dict["total_pages"]) > page_number:
-    #     count = int(init_dict["total_pages"])
-    #     print("This is count " + str(count))
-    #     for page_number in range(count):
-    #         loop_endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page={page_number}"
-    #         loop_req = requests.get(loop_endpoint)
-    #         loop_json = loop_req.text
-    #         loop_dict.update(loop_json)
-    #         final_dict.update(loop_dict["results"])
-    #         loop_dict.clear()
-    #         loop_json = ""
-    #         page_number += 1
-    #     return final_dict
-    # else:
-    return final_dict
+    final_dict = {}
+  # I have to stop here, but I am trying to get all of the pages here into one CSV form
+    if int(init_dict["total_pages"]) > page_number:
+        count = int(init_dict["total_pages"])
+        print("This is count " + str(count))
+        dict_stuff = []
+        trans_dict = {}
+        for x in range(count):
+            loop_endpoint = f"{api_base_url}{endpoint_path}?{api_key}&query={new_keyword}&page={x + 1}"
+            loop_req = requests.get(loop_endpoint)
+            loop_json = loop_req.text
+            loop_dict = json.loads(loop_json)
+            loop_json = ""
+            loop_req  = ""
+            trans_dict = loop_dict
+            dict_stuff.append(trans_dict.get("results"))
+            # final_dict.update({"results": "deez nuts"})
+            # final_dict.update({"results": dict_stuff.append(trans_dict.get("results"))})
+            trans_dict.clear()
+        final_dict.update({"results": dict_stuff})
+        return final_dict["results"]
+    else:
+        return init_dict["results"]
 
 # Here we are looking at the results only for a cleaner search, then saving them as a csv format to be looked at.
-def display_data(search_dict):
-    df = pd.DataFrame(search_dict).rem
+def display_data(search_list):
+    length = len(search_list)
+    new_table = pd.DataFrame()
+ # this looks at the length of the list, if it's large we will need to get all the data manageable.
+    if length > 1:
+        print("the if is running")
+        for x in range(length):
+            df = pd.DataFrame(search_list[x], columns=['adult', 'backdrop_path', 'genre_ids', 'id', 'original_language',
+                                                    'original_title', 'overview', 'popularity', 'poster_path',
+                                                    'release_date', 'title', 'video', 'vote_average', 'vote_count'])
+            print(df)
+            new_table = new_table.append(df)
+        file_export = new_table.to_csv()
+        return file_export
+    else:
+        df = pd.DataFrame(search_list[0], columns=['adult', 'backdrop_path','genre_ids', 'id', 'original_language',
+                                                   'original_title', 'overview', 'popularity', 'poster_path',
+                                                   'release_date', 'title', 'video', 'vote_average', 'vote_count'])
     file_export = df.to_csv()
     print(df)
     return file_export
